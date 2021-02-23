8a9f2a477911c6f8e328a7a894cdc0fb768dce06
phatdm-bap <phatdm@bap-ventures.com>
2021-02-23 17:54:56 +0700

    Update info for 2 modules GHTK & GHN

---
+++ delivery_ghtk/__manifest__.py
@@ -0,0 +1,17 @@
+# -*- coding: utf-8 -*-
+{
+    'name': 'Giao Hang Tiet Kiem',
+    'version': '1.1.0',
+    'category': 'Delivery GHTK',
+    'description': "This module build based on the structure of Giao Hang Tiet Kiem in Viet Nam.",
+    'author': 'phatdm@bap-ventures.com',
+    'depends': [
+        # Odoo
+        'base',
+        'delivery',
+    ],
+    'data': [],
+    'installable': True,
+    'application': False,
+    'auto_install': False,
+}
